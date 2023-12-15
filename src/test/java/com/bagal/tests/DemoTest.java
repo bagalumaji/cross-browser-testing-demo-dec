7b0d84859d649f37437d907785376fa9d3476636
bagalumaji <bagalumaji@gmail.com>
2023-12-15 20:56:34 +0530

    updated basic code test launch the browser

--- src/test/java/com/bagal/tests/DemoTest.java
+++ src/test/java/com/bagal/tests/DemoTest.java
@@ -1,10 +1,19 @@
 package com.bagal.tests;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
+
+import java.time.Duration;
 
 public class DemoTest {
     @Test
     public void demoTest(){
-
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.amazon.in/");
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(60));
+        System.out.println("driver.getTitle() = " + driver.getTitle());
+        driver.quit();
     }
 }
