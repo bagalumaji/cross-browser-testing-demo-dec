edad3911465bbb029c21ac4b4339350370f0bddb
bagalumaji <bagalumaji@gmail.com>
2023-12-15 21:07:50 +0530

    added code get the data from data provider

--- src/test/java/com/bagal/tests/DemoTest.java
+++ src/test/java/com/bagal/tests/DemoTest.java
@@ -2,18 +2,31 @@ package com.bagal.tests;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
 
 public class DemoTest {
-    @Test
-    public void demoTest(){
-        WebDriver driver = new ChromeDriver();
+    @Test(dataProvider = "getData")
+    public void demoTest(String username,String password,String browser){
+        /*WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://www.amazon.in/");
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(60));
         System.out.println("driver.getTitle() = " + driver.getTitle());
-        driver.quit();
+        driver.quit();*/
+        System.out.print("username = " + username);
+        System.out.print("  password = " + password);
+        System.out.println("  browser = " + browser);
+    }
+    @DataProvider
+    public Object[][] getData(){
+        Object[][] data={
+                {"username-1","password-1","chrome"},
+                {"username-2","password-1","firefox"},
+                {"username-3","password-3","edge"}
+        };
+        return data;
     }
 }
