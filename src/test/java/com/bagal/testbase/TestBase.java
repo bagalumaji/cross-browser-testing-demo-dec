d2331416e59063cbd04531ede17f056d1af974c5
bagalumaji <bagalumaji@gmail.com>
2023-12-16 20:42:55 +0530

    added test data file

--- src/test/java/com/bagal/testbase/TestBase.java
+++ src/test/java/com/bagal/testbase/TestBase.java
@@ -1,5 +1,6 @@
 package com.bagal.testbase;
 
+import com.bagal.models.TestData;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
@@ -9,14 +10,15 @@ import java.time.Duration;
 
 public class TestBase {
     protected WebDriver driver;
-    @BeforeMethod
-    public void setup(){
+    @BeforeMethod(alwaysRun = true)
+    public void setup(Object [] data){
+        TestData testData = (TestData) data[0];
         driver= new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://www.amazon.in/");
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(60));
     }
-    @AfterMethod
+    @AfterMethod(alwaysRun = true)
     public void tearDown(){
         driver.quit();
     }
