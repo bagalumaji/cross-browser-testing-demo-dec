49ea4cc0c152636e2ce8b6199600e7b47ccf6558
bagalumaji <bagalumaji@gmail.com>
2023-12-16 08:25:52 +0530

    updated with data provider with map

--- src/test/java/com/bagal/tests/DemoTest.java
+++ src/test/java/com/bagal/tests/DemoTest.java
@@ -16,26 +16,30 @@ import java.util.Map;
 public class DemoTest {
     @BeforeMethod(alwaysRun = true)
     public void setup(Object [] testData){
-        System.out.print("username = " + testData[0]);
+        /*System.out.print("username = " + testData[0]);
         System.out.print("  password = " + testData[1]);
-        System.out.println("  browser = " + testData[2]);
+        System.out.println("  browser = " + testData[2]);*/
+        Map<String,String> data = (Map)testData[0];
+        System.out.print("username = " + data.get("username"));
+        System.out.print("  password = " + data.get("password"));
+        System.out.println("  browser = " + data.get("browser"));
     }
 
     @Test(dataProvider = "getData")
-    public void demoTest(String username,String password,String browser){
+    public void demoTest(Map<String,String> data){
         /*WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://www.amazon.in/");
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(60));
         System.out.println("driver.getTitle() = " + driver.getTitle());
         driver.quit();*/
-        System.out.print("username = " + username);
-        System.out.print("  password = " + password);
-        System.out.println("  browser = " + browser);
+        System.out.print("username = " + data.get("username"));
+        System.out.print("  password = " + data.get("password"));
+        System.out.println("  browser = " + data.get("browser"));
     }
     @DataProvider
-    public Object[][] getData(){
-      /*  Map<String,String> map = new HashMap<>();
+    public Object[] getData(){
+       Map<String,String> map = new HashMap<>();
         map.put("username","username-1");
         map.put("password","password-1");
         map.put("browser","chrome");
@@ -50,11 +54,11 @@ public class DemoTest {
         map3.put("password","password-3");
         map3.put("browser","firefox");
 
-        return new Object[]{map,map2,map3};*/
-        return new Object[][]{
+        return new Object[]{map,map2,map3};
+        /*return new Object[][]{
                 {"username-1","password-1","chrome"},
                 {"username-2","password-1","firefox"},
                 {"username-3","password-3","edge"}
-        };
+        };*/
     }
 }
