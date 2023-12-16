1f591d0baae74c740bfadfbaddbec2e4574a3507
bagalumaji <bagalumaji@gmail.com>
2023-12-16 20:40:24 +0530

    added 2 tests

--- src/test/java/com/bagal/tests/amazonsearch/AmazonSearchTests.java
+++ src/test/java/com/bagal/tests/amazonsearch/AmazonSearchTests.java
@@ -1,11 +1,36 @@
 package com.bagal.tests.amazonsearch;
 
+import com.bagal.models.TestData;
 import com.bagal.testbase.TestBase;
+import io.github.sskorol.core.DataSupplier;
+import io.github.sskorol.data.XlsxReader;
+import one.util.streamex.StreamEx;
 import org.testng.annotations.Test;
 
+import java.lang.reflect.Method;
+
+import static io.github.sskorol.data.TestDataReader.use;
+
 public class AmazonSearchTests extends TestBase {
-    @Test
-    public void validSearchTest(){
+    @Test(dataProvider = "getData")
+    public void amazonSearchTest1(TestData testData) {
         System.out.println("driver.getTitle() = " + driver.getTitle());
+    }
+    @Test(dataProvider = "getData")
+    public void amazonSearchTest2(TestData testData) {
+        System.out.println("driver.getTitle() = " + driver.getTitle());
+    }
+    @Test(dataProvider = "getData")
+    public void amazonSearchTest3(TestData testData) {
+        System.out.println("driver.getTitle() = " + driver.getTitle());
+    }
+
+    @DataSupplier
+    public StreamEx<TestData> getData(Method method) {
+        return use(XlsxReader.class)
+                .withTarget(TestData.class)
+                .withSource("testdata.xlsx")
+                .read()
+                .filter(testdata -> testdata.getTestcase().equalsIgnoreCase(method.getName()));
     }
 }
