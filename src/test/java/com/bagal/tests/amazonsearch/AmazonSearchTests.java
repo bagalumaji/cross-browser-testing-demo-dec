2e3f3fb8a8d28f2d9501df6b9a6dba1ad5ab2bb1
bagalumaji <bagalumaji@gmail.com>
2023-12-17 21:57:26 +0530

    updated test

--- src/test/java/com/bagal/tests/amazonsearch/AmazonSearchTests.java
+++ src/test/java/com/bagal/tests/amazonsearch/AmazonSearchTests.java
@@ -1,5 +1,6 @@
 package com.bagal.tests.amazonsearch;
 
+import com.bagal.driver.DriverManager;
 import com.bagal.models.TestData;
 import com.bagal.testbase.TestBase;
 import io.github.sskorol.core.DataSupplier;
@@ -14,17 +15,17 @@ import static io.github.sskorol.data.TestDataReader.use;
 public class AmazonSearchTests extends TestBase {
     @Test(dataProvider = "getData")
     public void amazonSearchTest1(TestData testData) {
-        System.out.println("driver.getTitle() = " + driver.getTitle());
+        System.out.println("driver.getTitle() = " + DriverManager.getDriver().getTitle());
         System.out.println("testData = " + testData);
     }
     @Test(dataProvider = "getData")
     public void amazonSearchTest2(TestData testData) {
-        System.out.println("driver.getTitle() = " + driver.getTitle());
+        System.out.println("driver.getTitle() = " + DriverManager.getDriver().getTitle());
         System.out.println("testData = " + testData);
     }
     @Test(dataProvider = "getData")
     public void amazonSearchTest3(TestData testData) {
-        System.out.println("driver.getTitle() = " + driver.getTitle());
+        System.out.println("driver.getTitle() = " + DriverManager.getDriver().getTitle());
         System.out.println("testData = " + testData);
     }
 
