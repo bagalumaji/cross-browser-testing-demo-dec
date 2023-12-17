138496eee4698171f0464196636c7a53830e100c
bagalumaji <bagalumaji@gmail.com>
2023-12-17 22:52:07 +0530

    added locator and method for search

--- src/main/java/com/bagal/pages/HomePage.java
+++ src/main/java/com/bagal/pages/HomePage.java
@@ -1,4 +1,22 @@
 package com.bagal.pages;
 
-public class HomePage {
+import com.bagal.pageaction.PageAction;
+import org.openqa.selenium.By;
+
+import static com.bagal.pageaction.PageAction.click;
+import static com.bagal.pageaction.PageAction.type;
+
+public class HomePage extends BasePage{
+    private static final By SEARCH_TEXT_BOX = By.id("twotabsearchtextbox");
+    private static final  By SEARCH_BUTTON =By.id("nav-search-submit-button");
+    @Override
+    public boolean isPageLoaded() {
+        return PageAction.isDisplayed(SEARCH_TEXT_BOX);
+    }
+    public void typeTextInSearchTextBox(String searchText){
+        type(SEARCH_TEXT_BOX,searchText);
+    }
+    public void clickOnSearchButton(){
+        click(SEARCH_BUTTON);
+    }
 }
