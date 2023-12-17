0e545c6df5db977e7c26d9db61afeb88708e450c
bagalumaji <bagalumaji@gmail.com>
2023-12-17 21:13:19 +0530

    added setdriver, getdriver and unload method

--- src/main/java/com/bagal/driver/DriverManager.java
+++ src/main/java/com/bagal/driver/DriverManager.java
@@ -1,4 +1,26 @@
 package com.bagal.driver;
 
-public class DriverManager {
+import org.openqa.selenium.WebDriver;
+
+import java.util.Objects;
+
+public final class DriverManager {
+    private DriverManager() {
+    }
+
+    private static final ThreadLocal<WebDriver> WEB_DRIVER_THREAD_LOCAL = new ThreadLocal<>();
+
+    public static WebDriver getDriver() {
+        return WEB_DRIVER_THREAD_LOCAL.get();
+    }
+
+    public static void setDriver(WebDriver driver) {
+        if (Objects.nonNull(driver)) {
+            WEB_DRIVER_THREAD_LOCAL.set(driver);
+        }
+    }
+
+    public static void unload() {
+        WEB_DRIVER_THREAD_LOCAL.remove();
+    }
 }
