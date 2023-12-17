d1a8333c3e8f1bb419adf3016173aab14c40a66a
bagalumaji <bagalumaji@gmail.com>
2023-12-17 22:58:25 +0530

    removed 2 tests and updated 1st test

--- src/test/java/com/bagal/tests/amazonsearch/AmazonSearchTests.java
+++ src/test/java/com/bagal/tests/amazonsearch/AmazonSearchTests.java
@@ -1,25 +1,23 @@
 package com.bagal.tests.amazonsearch;
 
 import com.bagal.dataproviders.AmazonDataProvider;
-import com.bagal.driver.DriverManager;
 import com.bagal.models.TestData;
+import com.bagal.pageaction.PageAction;
+import com.bagal.pages.HomePage;
 import com.bagal.testbase.TestBase;
 import org.testng.annotations.Test;
 
+import static org.testng.Assert.assertTrue;
+
 public class AmazonSearchTests extends TestBase {
-    @Test(dataProvider = "getAmazonSearchData",dataProviderClass = AmazonDataProvider.class)
+    @Test(dataProvider = "getAmazonSearchData", dataProviderClass = AmazonDataProvider.class)
     public void amazonSearchTest1(TestData testData) {
-        System.out.println("driver.getTitle() = " + DriverManager.getDriver().getTitle());
+        System.out.println("PageAction.getPageTitle() = " + PageAction.getPageTitle());
         System.out.println("testData = " + testData);
-    }
-    @Test(dataProvider = "getAmazonSearchData",dataProviderClass = AmazonDataProvider.class)
-    public void amazonSearchTest2(TestData testData) {
-        System.out.println("driver.getTitle() = " + DriverManager.getDriver().getTitle());
-        System.out.println("testData = " + testData);
-    }
-    @Test(dataProvider = "getAmazonSearchData",dataProviderClass = AmazonDataProvider.class)
-    public void amazonSearchTest3(TestData testData) {
-        System.out.println("driver.getTitle() = " + DriverManager.getDriver().getTitle());
-        System.out.println("testData = " + testData);
+        HomePage homePage = new HomePage();
+        assertTrue(homePage.isPageLoaded());
+        homePage
+                .typeTextInSearchTextBox(testData.getSearchText())
+                .clickOnSearchButton();
     }
 }
