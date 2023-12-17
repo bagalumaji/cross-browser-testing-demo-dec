0646c1b2f07c8700f5870a15b530a742a357470a
bagalumaji <bagalumaji@gmail.com>
2023-12-17 21:23:41 +0530

    added initDriver method in driver

--- src/main/java/com/bagal/driver/Driver.java
+++ src/main/java/com/bagal/driver/Driver.java
@@ -1,4 +1,18 @@
 package com.bagal.driver;
 
-public class Driver {
+import com.bagal.factories.DriverFactory;
+
+import java.util.Objects;
+
+public final class Driver {
+    private Driver(){}
+
+    public static void initDriver(String browser){
+        if(Objects.isNull(DriverManager.getDriver())){
+            DriverManager.setDriver(DriverFactory.getDriver(browser));
+        }
+
+    }
+    public static void quitDriver(){}
+
 }
