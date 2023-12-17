b48efbc0576f0d0742691b6231aa66a1184dd8d6
bagalumaji <bagalumaji@gmail.com>
2023-12-17 22:43:37 +0530

    added method to wait for element clickable and visible

--- src/main/java/com/bagal/factories/WaitFactory.java
+++ src/main/java/com/bagal/factories/WaitFactory.java
@@ -1,4 +1,22 @@
 package com.bagal.factories;
 
+import com.bagal.driver.DriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
 public class WaitFactory {
+    public static WebElement waitForElementToBeClickable(By locator) {
+        return getWait().until(ExpectedConditions.elementToBeClickable(locator));
+    }
+    private static WebDriverWait getWait(){
+        return new WebDriverWait(DriverManager.getDriver(), Duration.ofSeconds(15));
+    }
+
+    public static WebElement waitForElementToVisible(By locator) {
+        return getWait().until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
 }
