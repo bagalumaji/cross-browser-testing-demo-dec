dbd097002f0429cef8e9f3450f503ec8d1b6512f
bagalumaji <bagalumaji@gmail.com>
2023-12-17 22:26:59 +0530

    updated data provider name

--- src/test/java/com/bagal/tests/amazonsearch/AmazonSearchTests.java
+++ src/test/java/com/bagal/tests/amazonsearch/AmazonSearchTests.java
@@ -1,40 +1,25 @@
 package com.bagal.tests.amazonsearch;
 
+import com.bagal.dataproviders.AmazonDataProvider;
 import com.bagal.driver.DriverManager;
 import com.bagal.models.TestData;
 import com.bagal.testbase.TestBase;
-import io.github.sskorol.core.DataSupplier;
-import io.github.sskorol.data.XlsxReader;
-import one.util.streamex.StreamEx;
 import org.testng.annotations.Test;
 
-import java.lang.reflect.Method;
-
-import static io.github.sskorol.data.TestDataReader.use;
-
 public class AmazonSearchTests extends TestBase {
-    @Test(dataProvider = "getData")
+    @Test(dataProvider = "getAmazonSearchData",dataProviderClass = AmazonDataProvider.class)
     public void amazonSearchTest1(TestData testData) {
         System.out.println("driver.getTitle() = " + DriverManager.getDriver().getTitle());
         System.out.println("testData = " + testData);
     }
-    @Test(dataProvider = "getData")
+    @Test(dataProvider = "getAmazonSearchData",dataProviderClass = AmazonDataProvider.class)
     public void amazonSearchTest2(TestData testData) {
         System.out.println("driver.getTitle() = " + DriverManager.getDriver().getTitle());
         System.out.println("testData = " + testData);
     }
-    @Test(dataProvider = "getData")
+    @Test(dataProvider = "getAmazonSearchData",dataProviderClass = AmazonDataProvider.class)
     public void amazonSearchTest3(TestData testData) {
         System.out.println("driver.getTitle() = " + DriverManager.getDriver().getTitle());
         System.out.println("testData = " + testData);
-    }
-
-    @DataSupplier(name = "getData")
-    public StreamEx<TestData> getData(Method method) {
-        return use(XlsxReader.class)
-                .withTarget(TestData.class)
-                .withSource("testdata.xlsx")
-                .read()
-                .filter(testdata -> testdata.getTestcase().equalsIgnoreCase(method.getName()));
     }
 }
