92917ba0bd2e910c841e84bcc71f7ff7b3bc47d8
bagalumaji <bagalumaji@gmail.com>
2023-12-17 22:52:32 +0530

    added isDisplayed method

--- src/main/java/com/bagal/pageaction/PageAction.java
+++ src/main/java/com/bagal/pageaction/PageAction.java
@@ -3,22 +3,30 @@ package com.bagal.pageaction;
 import com.bagal.driver.DriverManager;
 import com.bagal.factories.WaitFactory;
 import org.openqa.selenium.By;
+import org.openqa.selenium.TimeoutException;
 
 public final class PageAction {
     private PageAction() {
     }
 
 
-    public static void waitForElementAndClick(By locator) {
+    public static void click(By locator) {
         WaitFactory.waitForElementToBeClickable(locator).click();
     }
 
-    public static void waitForElementAndType(By locator, String message) {
+    public static void type(By locator, String message) {
         WaitFactory.waitForElementToVisible(locator).sendKeys(message);
     }
 
     public static String getPageTitle() {
         return DriverManager.getDriver().getTitle();
+    }
+    public static boolean isDisplayed(By locator){
+        try {
+            return WaitFactory.waitForElementToVisible(locator).isDisplayed();
+        }catch (TimeoutException e){
+            return false;
+        }
     }
 
 }
