d15b290c78836e33aaaf3fb0ca30fa1469c71f1e
bagalumaji <bagalumaji@gmail.com>
2023-12-17 21:26:37 +0530

    updated test base with driver method

--- src/test/java/com/bagal/testbase/TestBase.java
+++ src/test/java/com/bagal/testbase/TestBase.java
@@ -1,25 +1,18 @@
 package com.bagal.testbase;
 
+import com.bagal.driver.Driver;
 import com.bagal.models.TestData;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
-import java.time.Duration;
-
 public class TestBase {
-    protected WebDriver driver;
     @BeforeMethod(alwaysRun = true)
     public void setup(Object [] data){
         TestData testData = (TestData) data[0];
-        driver= new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.get("https://www.amazon.in/");
-        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(60));
+        Driver.initDriver(testData.getBrowser());
     }
     @AfterMethod(alwaysRun = true)
     public void tearDown(){
-        driver.quit();
+        Driver.quitDriver();
     }
 }
