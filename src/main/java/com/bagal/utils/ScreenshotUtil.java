55c09136c217a377958096f5b08aeaa33c1f798f
bagalumaji <bagalumaji@gmail.com>
2023-12-17 22:27:32 +0530

    added getscreenshot method

--- src/main/java/com/bagal/utils/ScreenshotUtil.java
+++ src/main/java/com/bagal/utils/ScreenshotUtil.java
@@ -1,4 +1,12 @@
 package com.bagal.utils;
 
-public class ScreenshotUtil {
+import com.bagal.driver.DriverManager;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+public final class ScreenshotUtil {
+    private ScreenshotUtil(){}
+    public static String getScreenshotAsBase64String(){
+        return ((TakesScreenshot) DriverManager.getDriver()).getScreenshotAs(OutputType.BASE64);
+    }
 }
