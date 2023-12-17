959327e9e850a5bb45757945df9759e7b7540278
bagalumaji <bagalumaji@gmail.com>
2023-12-17 21:24:01 +0530

    added getdriver method

--- src/main/java/com/bagal/factories/DriverFactory.java
+++ src/main/java/com/bagal/factories/DriverFactory.java
@@ -1,4 +1,27 @@
 package com.bagal.factories;
 
-public class DriverFactory {
+import com.bagal.exceptions.BrowserNotSupportedException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
+import java.time.Duration;
+
+public final class DriverFactory {
+    private DriverFactory() {
+    }
+
+    public static WebDriver getDriver(String browser) {
+        WebDriver driver = switch (browser) {
+            case "chrome" -> new ChromeDriver();
+            case "firefox" -> new FirefoxDriver();
+            case "safari" -> new SafariDriver();
+            default -> throw new BrowserNotSupportedException(browser + " browser not supported");
+        };
+        driver.manage().window().maximize();
+        driver.get("https://www.amazon.in/");
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(60));
+        return driver;
+    }
 }
