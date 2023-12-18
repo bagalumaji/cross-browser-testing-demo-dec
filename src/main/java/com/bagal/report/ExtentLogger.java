6a887952d148204111d78e5d3f59a7f674370059
bagalumaji <bagalumaji@gmail.com>
2023-12-18 21:39:03 +0530

    added logger method as pass, fail skip and info

--- src/main/java/com/bagal/report/ExtentLogger.java
+++ src/main/java/com/bagal/report/ExtentLogger.java
@@ -1,4 +1,24 @@
 package com.bagal.report;
 
-public class ExtentLogger {
+import com.aventstack.extentreports.MediaEntityBuilder;
+import com.bagal.utils.ScreenshotUtil;
+
+public final class ExtentLogger {
+    private ExtentLogger(){}
+
+    public static void pass(String message){
+        ExtentManager.getExtentTest().pass(message);
+    }
+    public static void fail(String message){
+        ExtentManager.getExtentTest().fail(message, MediaEntityBuilder.createScreenCaptureFromBase64String(ScreenshotUtil.getScreenshotAsBase64String()).build());
+    }
+
+    public static void skip(String message){
+        ExtentManager.getExtentTest().skip(message);
+
+    }
+    public static void info(String message){
+        ExtentManager.getExtentTest().info(message);
+
+    }
 }
