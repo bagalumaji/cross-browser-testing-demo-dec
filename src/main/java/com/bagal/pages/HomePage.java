4445afb4a743ef67648c7301396d153873cc714a
bagalumaji <bagalumaji@gmail.com>
2023-12-18 21:40:22 +0530

    added logs

--- src/main/java/com/bagal/pages/HomePage.java
+++ src/main/java/com/bagal/pages/HomePage.java
@@ -3,6 +3,7 @@ package com.bagal.pages;
 import com.bagal.constants.HomePageConstants;
 import com.bagal.factories.WaitFactory;
 import com.bagal.pageaction.PageAction;
+import com.bagal.report.ExtentLogger;
 import com.bagal.utils.CreateXpathUtil;
 import org.openqa.selenium.By;
 
@@ -19,16 +20,20 @@ public class HomePage extends BasePage{
     }
     public HomePage typeTextInSearchTextBox(String searchText){
         type(SEARCH_TEXT_BOX,searchText);
+        ExtentLogger.info("entered text into search box as : "+searchText);
         return this;
     }
     public HomePage clickOnSearchButton(){
         click(SEARCH_BUTTON);
+        ExtentLogger.info("clicked on search button");
         return this;
     }
     public boolean isDisplayedProductCategory(String productCategory){
+        ExtentLogger.info("checking for : "+productCategory+ " is displayed or not");
         return isDisplayed(CreateXpathUtil.getXpath(HomePageConstants.getXpathProductCategory(),productCategory));
     }
     public boolean isDisplayedSearchResultWithExpectedCount(int count){
+        ExtentLogger.info("checking for search result count is displayed or not");
         return WaitFactory.waitForVisibilityOfAllElements(SEARCH_RESULT).size()>count;
     }
 }
