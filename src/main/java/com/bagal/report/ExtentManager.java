d43151cee4d45391d7e96e386b18e6eb65c98d6f
bagalumaji <bagalumaji@gmail.com>
2023-12-18 20:48:37 +0530

    added code for multithreading support

--- src/main/java/com/bagal/report/ExtentManager.java
+++ src/main/java/com/bagal/report/ExtentManager.java
@@ -1,4 +1,23 @@
 package com.bagal.report;
 
-public class ExtentManager {
+import com.aventstack.extentreports.ExtentTest;
+
+import java.util.Objects;
+
+public final class ExtentManager {
+    private ExtentManager(){}
+
+    private static final ThreadLocal<ExtentTest> EXTENT_TEST = new ThreadLocal<>();
+
+    public static void setExtentTest(ExtentTest test){
+        if(Objects.nonNull(test)){
+            EXTENT_TEST.set(test);
+        }
+    }
+    public static ExtentTest getExtentTest(){
+        return  EXTENT_TEST.get();
+    }
+    public static void unload(){
+        EXTENT_TEST.remove();
+    }
 }
