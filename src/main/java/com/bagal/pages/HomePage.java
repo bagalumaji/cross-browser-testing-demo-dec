fc385de51a9f63326703ba417d2fa50bfadf7ece
bagalumaji <bagalumaji@gmail.com>
2023-12-18 20:07:49 +0530

    updated homepage

--- src/main/java/com/bagal/pages/HomePage.java
+++ src/main/java/com/bagal/pages/HomePage.java
@@ -1,14 +1,18 @@
 package com.bagal.pages;
 
+import com.bagal.constants.HomePageConstants;
+import com.bagal.factories.WaitFactory;
 import com.bagal.pageaction.PageAction;
+import com.bagal.utils.CreateXpathUtil;
 import org.openqa.selenium.By;
 
-import static com.bagal.pageaction.PageAction.click;
-import static com.bagal.pageaction.PageAction.type;
+import static com.bagal.pageaction.PageAction.*;
 
 public class HomePage extends BasePage{
     private static final By SEARCH_TEXT_BOX = By.id("twotabsearchtextbox");
     private static final  By SEARCH_BUTTON =By.id("nav-search-submit-button");
+
+    private static final By SEARCH_RESULT =By.xpath("//div[@data-component-type='s-search-result']");
     @Override
     public boolean isPageLoaded() {
         return PageAction.isDisplayed(SEARCH_TEXT_BOX);
@@ -20,5 +24,11 @@ public class HomePage extends BasePage{
     public HomePage clickOnSearchButton(){
         click(SEARCH_BUTTON);
         return this;
+    }
+    public boolean isDisplayedProductCategory(String productCategory){
+        return isDisplayed(CreateXpathUtil.getXpath(HomePageConstants.getXpathProductCategory(),productCategory));
+    }
+    public boolean isDisplayedSearchResultWithExpectedCount(int count){
+        return WaitFactory.waitForVisibilityOfAllElements(SEARCH_RESULT).size()>count;
     }
 }
