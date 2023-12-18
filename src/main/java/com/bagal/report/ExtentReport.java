023f80bb77af4e1309f25cb441b377f9312c22d5
bagalumaji <bagalumaji@gmail.com>
2023-12-18 20:49:12 +0530

    added method to initreport, createtest and flush report

--- src/main/java/com/bagal/report/ExtentReport.java
+++ src/main/java/com/bagal/report/ExtentReport.java
@@ -1,18 +1,33 @@
 package com.bagal.report;
 
-public final class ExtentReport {
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
 
+import java.util.Objects;
+
+public final class ExtentReport {
+private static ExtentReports extentReports;
     private ExtentReport() {
     }
 
     public static void initReport() {
-
+        extentReports = new ExtentReports();
+        ExtentSparkReporter extentSparkReporter = new ExtentSparkReporter("reports/index.html");
+        extentSparkReporter.config().setTheme(Theme.DARK);
+        extentSparkReporter.config().setReportName("Amazon Search Test Report");
+        extentSparkReporter.config().setDocumentTitle("Cross Browser Test");
+        extentReports.attachReporter(extentSparkReporter);
     }
 
-    public static void createTest() {
+    public static void createTest(String testCaseName) {
+        ExtentManager.setExtentTest(extentReports.createTest(testCaseName));
     }
 
     public static void flushReport() {
-
+        if(Objects.nonNull(extentReports)){
+            extentReports.flush();
+            ExtentManager.unload();
+        }
     }
 }
